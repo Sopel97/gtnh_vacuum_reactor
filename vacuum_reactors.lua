66e413f4457ecd5f5d46d8545d6dd96b0bd6c5a8
Eldrinn-Elantey <46845681+Eldrinn-Elantey@users.noreply.github.com>
2024-12-30 20:59:08 +0400

    Color indication for true and false

--- vacuum_reactors.lua
+++ vacuum_reactors.lua
@@ -880,8 +880,22 @@ local function create_widgets(lsc, reactors)
             gpu.set(widget.min_x + 2, widget.min_y + 2, "LSC: " .. tostring(lsc_status.used_capacity_eu) .. "EU / " .. tostring(lsc_status.total_capacity_eu) .. "EU   (" .. tostring(lsc_fill_pct) .. "%)")
             gpu.set(widget.min_x + 2, widget.min_y + 3, "Passive loss: " .. tostring(lsc_status.passive_loss_eut) .. "EU/t")
             gpu.set(widget.min_x + 2, widget.min_y + 4, "I/O [EU/t]: +" .. tostring(lsc_status.avg_input_eut) .. " -" .. tostring(lsc_status.avg_output_eut) .. " -" .. tostring(lsc_status.passive_loss_eut) .. " = " .. tostring(average_net_input_eut) .. "EU/t" .. empty_or_full_message)
-            gpu.set(widget.min_x + 2, widget.min_y + 5, "LSC needs maintenenance: " .. tostring(lsc_status.needs_maintenance))
-            gpu.set(widget.min_x + 2, widget.min_y + 6, "LSC needs powergen: " .. tostring(lsc.needs_powergen))
+	        gpu.set(widget.min_x + 2, widget.min_y + 5, "LSC needs maintenance: ")
+		        if lsc_status.needs_maintenance then
+  	                gpu.setForeground(0xFF0000)
+                else
+                    gpu.setForeground(0x00FF00)
+                end
+            gpu.set(widget.min_x + 25, widget.min_y + 5, tostring(lsc_status.needs_maintenance))
+            gpu.setForeground(0xFFFFFF)
+            gpu.set(widget.min_x + 2, widget.min_y + 6, "LSC needs powergen: ")
+                if lsc.needs_powergen then
+                    gpu.setForeground(0xFF0000)
+                else
+                    gpu.setForeground(0x00FF00)
+                end
+                gpu.set(widget.min_x + 23, widget.min_y + 6, tostring(lsc.needs_powergen))
+                gpu.setForeground(0xFFFFFF)
         end
     }
     table.insert(widgets, lsc_widget)
